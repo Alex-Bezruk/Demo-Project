2e8c585c2459c365c0cf36e75702ee807919a070
Alex Bezruk <oleksandr.bezruk@gmail.com>
2023-12-26 12:32:44 +0200

    Update write_key.sh

--- write_key.sh
+++ write_key.sh
@@ -17,15 +17,3 @@ decoded_value=$(echo "$aws_ssm_key_value" | base64 -d)
 echo "$decoded_value" > key.pem
 
 echo "Decoded value has been written to key.pem."
-
-
-host_parameter_name="/github-action-runners/gh-runner/runners/app_instance_host"
-deploy_host=$(aws ssm get-parameter --name "$host_parameter_name" --query "Parameter.Value" --output text)
-export DEPLOY_HOST="$deploy_host"
-
-user_parameter_name="/github-action-runners/gh-runner/runners/app_instance_user"
-deploy_user=$(aws ssm get-parameter --name "$user_parameter_name" --query "Parameter.Value" --output text)
-export DEPLOY_USERNAME="$deploy_user"
-
-echo "DEPLOY_HOST has been set to: $DEPLOY_HOST"
-echo "DEPLOY_USER has been set to: $DEPLOY_USERNAME"
