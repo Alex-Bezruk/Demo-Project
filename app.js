02ca7bf8c6e20ce63306cec606884a9530e25598
Alex Bezruk <oleksandr.bezruk@gmail.com>
2023-12-26 13:51:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('App Was Redeployed\n');
+  res.end('Hello World\n');
 });
 
 const port = 3000;
